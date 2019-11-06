e3ce4883dcb46471e6fcb712c169dffaeb33c013
Mike Poirier <mpoirier@MVP.local>
2019-11-06 11:58:22 +0400

    binary trees

--- AbstractDataTypes/src/BinaryTree.java
+++ AbstractDataTypes/src/BinaryTree.java
@@ -1,37 +1,71 @@
 /*
- * Developing a basic implementation of a BinaryTree.
+ * Developing a basic implementation of a Binary Tree.
+ * Source Video: https://www.youtube.com/watch?v=oSWTXtMglKE
  */
 
 public class BinaryTree {
-	Node root;
-	
-	public BinaryTree () {
-		root = null;
+	public static void main (String[] args) {
+		BinaryIntTree myTree = new BinaryIntTree(5);
+		myTree.insert(4);
+		myTree.insert(6);
+		System.out.println(myTree.contains(1));
+		System.out.println(myTree.contains(6));
+		myTree.printInOrder();
 	}
-	
-	public BinaryTree (int[] sortedArray) {
-		Node curr;
-		root = new Node (this.root, sortedArray[0]);
-		
-		for (int i = 0; i < sortedArray.length; i++) {
-			// create child nodes... not sure...
-		}
-		
-		
-	}
-
 }
 
-class Node {
-	Node parent;
-	Node leftChild;
-	Node rightChild;
+class BinaryIntTree{
+	
+	// Treat each binary tree as the root of a subtree of left and right children
+	BinaryIntTree left, right;
 	int data;
 	
-	public Node (Node p, int d) {
-		this.parent = p;
-		this.data = d;
-		this.leftChild = null;
-		this.rightChild = null;
+	public BinaryIntTree (int data) {
+		this.data = data;
+	}
+	
+	// Recursively insert 'value' into the tree, in correct order
+	public void insert (int value) {
+		if (value <= data) {
+			if (left == null) {
+				left = new BinaryIntTree(value);
+			} else {
+				left.insert(value);
+			}
+		} else {
+			if (right == null) {
+				right = new BinaryIntTree(value);
+			} else {
+				right.insert(value);
+			}
+		}
+	}
+	
+	// Recursively search for 'value' in the tree
+	public boolean contains (int value) {
+		if (value == data) {
+			return true;
+		} else if (value < data) {
+			if (left == null) {
+				return false;
+			} else {
+				return left.contains(value);
+			}
+		} else if (right == null) {
+			return false;
+		} else {
+			return right.contains(value);
+		}
+	}
+	
+	// Recursively print using an In Order Traversal
+	public void printInOrder () {
+		if (left != null) {
+			left.printInOrder();
+		}
+		System.out.println(data);
+		if (right != null) {
+			right.printInOrder();
+		}
 	}
 }
