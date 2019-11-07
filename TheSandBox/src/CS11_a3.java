7ead4e4f7444912e8caab10fec669f25ae55d7fd
Mike Poirier <mpoirier@MVP.local>
2019-11-07 14:47:35 +0400

    assignment

--- TheSandBox/src/CS11_a3.java
+++ TheSandBox/src/CS11_a3.java
@@ -8,6 +8,7 @@
 
 import java.util.Scanner;
 import java.math.*;
+import java.util.Arrays;
 
 public class CS11_a3 {
 
@@ -15,17 +16,26 @@ public class CS11_a3 {
 		
 		// initialize variables
 		int min = 0;
-		int max = 100;
-		int nums[] = new int[25]; // array of size 25
+		int max = 99;
+		int nums[] = new int[100]; // array of size 25
 
-		// create an array of random numbers between max and min
+		// create an array of random numbers between max and min (inclusive)
 		for (int i = 0; i < nums.length; i++) {
-			nums[i] = (int) (Math.random() * (max-min)) + min;
+			nums[i] = (int) (Math.random() * (max-min) + 1) + min;
 		}
 
 		// print array on a line, separated by a space
 		for (int i = 0; i < nums.length; i++) {
+				
+				if (nums[i] < 10) {
+					System.out.print("0"); // append a leading '0' to numbers less than 10
+				}
+			
 				System.out.print(nums[i] + " ");
+				
+				if ((i+1) % 25 == 0) {
+					System.out.println(); // add a new line after 25 elements
+				}
 		}
 		
 		/*
@@ -37,7 +47,7 @@ public class CS11_a3 {
 				count++;
 			}
 		}
-		System.out.println("Numbers between 50 and 60: " + count);
+		System.out.println("\nNumbers between 50 and 60: " + count);
 		
 		/*
 		 * MEDIUM: State the index of each value found
@@ -48,5 +58,35 @@ public class CS11_a3 {
 				System.out.print(i + " ");
 			}
 		}
+		
+		/*
+		 * HARD: Histogram of values between ranges of 10 (e.g. 10-20, 20-30)
+		 */
+		System.out.println("\n\n***HISTOGRAM***");
+		int[] bins = new int[10];
+		for (int i = 0; i < nums.length; i++) {
+			bins[(int) (nums[i]/10)]++; // divide each num by 10, increment bin counter
+		}
+		
+		// print ASCII representation of a horizontal histogram
+		for (int i = 0; i < bins.length; i++) {
+			System.out.print((i*10) + "-" + ((i+1) * 10) + ":\t"); // range title
+			for (int j = 0; j < bins[i]; j++) {
+				System.out.print("x"); // print an x for each count
+			}
+			System.out.println();
+		}
+		
+		// confirm total counts from historgram
+		int total = 0;
+		for (int i = 0; i < bins.length; i++) {
+			total = total + bins[i];
+		}
+		System.out.println("\nTotal counts: " + total);
+		System.out.println("Bins: " + Arrays.toString(bins));
+		
+		// exit successfully
+		System.out.println("\n*** Program Completed Successfully ***");
+		System.exit(0);
 	}
 }
