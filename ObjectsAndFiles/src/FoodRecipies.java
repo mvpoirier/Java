4ce10d3455d899df1f3e1b48729b9eb6cd1e58dd
Mike Poirier <mpoirier@MVP.local>
2019-11-10 12:53:12 +0400

    printCost

--- ObjectsAndFiles/src/FoodRecipies.java
+++ ObjectsAndFiles/src/FoodRecipies.java
@@ -8,38 +8,43 @@ public class FoodRecipies {
 
 	public static void main(String[] args) {
 		// main method that will run "java FoodRecipies args[0] args[1] ..."
-		
+
 		Recipie pasta; // create a pointer/variable of type Recipie
-		pasta = new Recipie ("Spaghetti", 4, 4); // pasta in an instance of the "Recipie" class.
-		
-		Recipie chicken = new Recipie ("Chinese Chicken", 5, 3);
-		
+		pasta = new Recipie("Spaghetti", 4, 4); // pasta in an instance of the "Recipie" class.
+
+		Recipie chicken = new Recipie("Chinese Chicken", 5, 3);
+
 		pasta.ingredients[0] = "pasta";
 		pasta.ingredients[1] = "tomato sauce";
 		pasta.ingredients[2] = "cheese";
 		pasta.ingredients[3] = "oregano";
-		
-		
+
 		for (int i = 0; i < pasta.ingredients.length; i++) {
 			System.out.println(pasta.ingredients[i]);
 		}
-		
-		//TODO: Import recipies from a text file ("recipies.txt")
-		
+
+		// TODO: Import recipies from a text file ("recipies.txt")
+
 	}
 
 }
 
 class Recipie {
-	String name;
-	String[] ingredients;
-	String[] steps;
-	int cost;
-	
-	public Recipie (String name, int numI, int numS) {
+	public String name;
+	public String[] ingredients;
+	public String[] steps;
+	private int cost;
+
+	public Recipie(String name, int numI, int numS) {
 		// constructor
 		this.name = name;
 		this.ingredients = new String[numI];
 		this.steps = new String[numS];
+	}
+
+	public String printCost() {
+		String totalCost;
+		totalCost = this.cost + ".00 OMR";
+		return totalCost;
 	}
 }
