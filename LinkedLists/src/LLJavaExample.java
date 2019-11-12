d8a41d7e4510ac769841ca17bc41e34b4e83ea8b
Mike Poirier <mpoirier@MVP.local>
2019-11-12 12:21:17 +0400

    LL Java Implementation

---
+++ LinkedLists/src/LLJavaExample.java
@@ -0,0 +1,28 @@
+/*
+ * An example of how to use Java's implementation
+ * of a Linked List.
+ * 
+ * @author	Mr. Poirier
+ * @version 1.1
+ * @since	2019-11-12
+ */
+
+import java.util.LinkedList;
+
+public class LLJavaExample {
+
+	public static void main(String[] args) {
+		LinkedList<String> myShoppingList = new LinkedList<String>();
+		
+		System.out.println(myShoppingList.isEmpty());
+		
+		myShoppingList.add("Apples");
+		myShoppingList.add("Soda Water");
+		myShoppingList.add("Toothbrush");
+		
+		System.out.println(myShoppingList.remove());
+		System.out.println(myShoppingList.removeLast());
+		
+		System.out.println(myShoppingList.isEmpty());
+	}
+}
