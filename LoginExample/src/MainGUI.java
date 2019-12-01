f91349e51fe7bb3c17e7e7d71df63eb71202e2e1
Mike Poirier <mpoirier@MVP.local>
2019-12-01 06:54:33 +0400

    GUI changes

--- LoginExample/src/MainGUI.java
+++ LoginExample/src/MainGUI.java
@@ -7,17 +7,17 @@
  */
 
 import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
- * 
- * CLASS:	MainGUI	
+ *
+ * CLASS:	MainGUI
  * PURPOSE:	Application Window (main method), using WindowBuilder
  */
 public class MainGUI {
@@ -56,22 +56,22 @@ public class MainGUI {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		frame.setTitle("MainGUI - Login Screen");
-		
+
 		txtUser = new JTextField();
 		txtUser.setBounds(156, 122, 130, 26);
 		frame.getContentPane().add(txtUser);
 		txtUser.setColumns(10);
-		
+
 		JLabel lblEnterUsername = new JLabel("Enter username:");
 		lblEnterUsername.setBounds(168, 85, 118, 16);
 		frame.getContentPane().add(lblEnterUsername);
-		
+
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setBounds(166, 160, 117, 29);
 		frame.getContentPane().add(btnLogin);
-		
+
 		/*
 		 * WHEN I CLICK THE LOGIN BUTTON
 		 */
